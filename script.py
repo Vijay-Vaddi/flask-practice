2e3b1062e600af72d1e5f6a3905f5dda1da5eb33
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-01-02 13:33:39 +0000

    basic template render

---
+++ script.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('basic.html')
+
+#render basic template from templates and data from static folder. 
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
--- test.py
+++
@@ -1,3 +0,0 @@
-print(f'Let\'s begin practicing and learing Flask for building cool websites')
-print('test 2')
- 
