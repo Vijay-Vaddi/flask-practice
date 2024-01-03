14361567f977cbe53487a5ac223c47c2e91ce84f
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-01-03 20:19:58 +0000

    custom 404 render

--- template_forms.py
+++ template_forms.py
@@ -19,6 +19,10 @@ def puppies_thankyou():
 
     return render_template('./puppies/puppies_thankyou.html' , first=first, last=last)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('./puppies/puppies_404.html'), 404
+
 if __name__ == '__main__':
     app.run(debug=True)
 
