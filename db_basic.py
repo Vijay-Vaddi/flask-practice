81f3766b8ad716ab32c40d2669ec8121aaac1061
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-01-15 18:20:22 +0000

    db migrate adding new column

--- db_basic.py
+++ db_basic.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
+from flask_migrate import Migrate
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -9,24 +9,25 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# class Base(DeclarativeBase):
-#     pass
 
 db = SQLAlchemy()  
 db.init_app(app)
+Migrate(app,db)
 app.app_context().push() 
 # needed to be called from outside
 
 class Puppy(db.Model):
-    # __tablename__ = 'puppies'
+    __tablename__ = 'puppies'   
 
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.Text)
     age = db.Column(db.Integer)
+    breed = db.Column(db.Text)
 
-    def __init__(self, name,age) -> None:
+    def __init__(self, name,age, breed) -> None:
         self.name = name
         self.age = age
+        self.breed = breed
 
     def __repr__(self) -> str:
         return f"Puppy {self.name} is {self.age} years old "
